7a0874afb340e4c0ac7588a8b0d48370be02ab1b
Cegaiel <sharpguy32@gmail.com>
2025-05-23 15:27:58 -0400

    Add Plane Keys for Collector Chest

--- scripts/chest_spin_x10.lua
+++ scripts/chest_spin_x10.lua
@@ -4,14 +4,15 @@ waitTime = 1500; -- wait time for things that do not have a SKIP checkbox
 
 function doit()
 
-	askForWindow("Spam click the Draws x10 button on Dragon and Frozen Chests event when you have lots of keys.\n\nDragon Chest Event: Check the 'Skip' checkbox to go faster!\n\nMouse over Blue Stacks window and press Shift key to continue.");  
+	askForWindow("Spam click the Draws x10 button on Dragon and Frozen Chests or Collectors Chest (plane keys) event when you have lots of keys.\n\nDragon Chest Event: Check the 'Skip' checkbox to go faster!\n\nMouse over Blue Stacks window and press Shift key to continue.");  
 	
   checkWindowSize();
 
   while 1 do
-
+      srReadScreen();
       local key = srFindImage("dragon_keys_small.png");
       local frozen = srFindImage("frozen_crystals.png");
+      local planes = srFindImage("planes_key_10.png");
 
 	if key then
           srClickMouse(key[0],key[1]);
@@ -21,9 +22,13 @@ function doit()
           srClickMouse(frozen[0],frozen[1]);
           sleepWithStatus(750, "Clicking Spin x1 button", nil, 0.7)
           sleepWithStatus(750, "CLICKED Spin x1 button", nil, 0.7)
+	elseif planes then
+          srClickMouse(planes[0],planes[1]);
+          sleepWithStatus(250, "Clicking Spin x1 button", nil, 0.7)
+          sleepWithStatus(250, "CLICKED Spin x1 button", nil, 0.7)
 	end
 
-	if not key and not frozen then
+	if not key and not frozen and not planes then
           sleepWithStatus(2000, "Could not anything to click, giving up", nil, 0.7)
           break;
         end
