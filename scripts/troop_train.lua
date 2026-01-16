d02d11b5bd5e22b34d2decb2f10db0c0d80a1a9c
Cegaiel <sharpguy32@gmail.com>
2026-01-16 05:30:57 -0500

    Tweak Troop training... Integrate newer common_ui.inc
    Add some new supporting files.

--- scripts/troop_train.lua
+++ scripts/troop_train.lua
@@ -2,58 +2,54 @@ dofile("common.inc");
 dofile("constants.inc");
 
 waitTime = 200; -- How many ms (1000 = 1 second) to wait for an image to appear before giving up
-tolerance = 4500; -- Image tolerance to find it -- Default is 4500 when no tolerance is specified
+defaultSleep = 1250; -- How many ms for most sleep
+tolerance = 6000; -- Image tolerance to find it -- Default is 4500 when no tolerance is specified
+checkedGray = 0;
+abort = 0;
+skipSliderBar = 0;
 
 function doit()
-  askForWindow("Troop Training\n\nSelect the troop you are training so that the BLUE TRAIN button is showing BEFORE you begin!\n\nMouse over Blue Stacks window and press Shift key to continue.");  
+  num_train = promptNumber("Num times (loops) to Train?", 1);
+  if promptOkay("Are you using One Time Recruitment (5000, 20000, 100000, 1000000) scrolls?\n\nYes: Don't click slider bars since scrolls force the max troops already; saves time!\n\nNo: Will click the right side of the slider bar, each loop, to set Max troops.", nil, 0.7, true) then
+    skipSliderBar = 1;
+  end
+  askForWindow("Troop Training\n\nSelect the troops types you are training.\nBe sure the BLUE TRAIN button is showing BEFORE you begin!\n\nMouse over Blue Stacks window and press Shift key to continue.");  
   checkWindowSize();
 
   if waitForImage("training/train.png", waitTime, "Verifying Blue Train button is showing") then
-    sleepWithStatus(1500, "Green Upgrade button found, continuing...", nil, 0.7)
+    sleepWithStatus(1500, "Blue Train button found, continuing...", nil, 0.7)
     can_continue = 1;
   else
     sleepWithStatus(2000, "Blue Train button NOT found, Shutting down!", nil, 0.7)
   end
 
 
-  while can_continue do
+  while can_continue and abort == 0 do
     checkBreak();
-
 
     -- Check Slider Bar before clicking Level up
     if waitForImage("training/gold_plus.png", waitTime, "Looking for Slider Bar/Plus button", nil, tolerance) then
       local gold_plus = srFindImage("training/gold_plus.png");
-      if gold_plus then
+      if gold_plus and skipSliderBar == 0 then
         srClickMouse(gold_plus[0]-71,gold_plus[1]+10);
         lsSleep(100);
         srClickMouse(gold_plus[0]-71,gold_plus[1]+10);
-       sleepWithStatus(100, "Clicking Max on Slider Bar", nil, 0.7)
+	    sleepWithStatus(defaultSleep, "Clicking Max on Slider Bar", nil, 0.7, "Loops remaining: " .. num_train);
       end
     end
 
     if waitForImage("training/train.png", waitTime, "Verifying Blue Train button is showing", nil, tolerance) then
       local train = srFindImage("training/train.png");
       srClickMouse(train[0],train[1]);
-      sleepWithStatus(100, "Clicking Upgrade button", nil, 0.7)
+      sleepWithStatus(defaultSleep, "Clicking Train button", nil, 0.7, "Loops remaining: " .. num_train)
     end
-
-    if waitForImage("training/level_up.png", waitTime, "Waiting for Level Up button", nil, tolerance) then
-      local level_up = srFindImage("training/level_up.png");
-      if level_up then
-        srClickMouse(level_up[0],level_up[1]);
-        sleepWithStatus(100, "Clicking Level-Up button", nil, 0.7)
-      end
-    end
-
-  --checkGrayButtons()
-
 
     -- Check for Blue Use All button. This will appear if we don't have enough resources at hand
     if waitForImage("training/use_all_blue.png", waitTime, "Checking for USE ALL button", nil, tolerance) then
       local blue_use_all = srFindImage("training/use_all_blue.png");
       if blue_use_all then
         srClickMouse(blue_use_all[0],blue_use_all[1]);
-        sleepWithStatus(100, "Clicking Blue: Use All button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Blue: Use All button", nil, 0.7, "Loops remaining: " .. num_train)
       end
     end
 
@@ -62,7 +58,7 @@ function doit()
       local green_use_all = srFindImage("training/use_all_green.png");
       if green_use_all then
         srClickMouse(green_use_all[0],green_use_all[1]);
-        sleepWithStatus(100, "Clicking Green: Use All button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Green: Use All button", nil, 0.7, "Loops remaining: " .. num_train)
       end
     end
 
@@ -72,7 +68,7 @@ function doit()
       local blue_speed_up = srFindImage("training/blue_speedup.png");
       if blue_speed_up then
         srClickMouse(blue_speed_up[0],blue_speed_up[1]);
-        sleepWithStatus(100, "Clicking Blue: Speed Up button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Blue: Speed Up button", nil, 0.7, "Loops remaining: " .. num_train)
       end
     end
 
@@ -81,7 +77,7 @@ function doit()
       local confirm_yes = srFindImage("training/confirm_yes.png");
       if confirm_yes then
         srClickMouse(confirm_yes[0],confirm_yes[1]);
-        sleepWithStatus(100, "Clicking Confirm Yes button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Confirm Yes button", nil, 0.7, "Loops remaining: " .. num_train)
       end
     end
   end -- while
@@ -92,47 +88,57 @@ function checkClaimGreen()
       local green_claim = srFindImage("training/claim_green.png");
       if green_claim then
         srClickMouse(green_claim[0],green_claim[1]);
-        sleepWithStatus(100, "Clicking Green: Claim All button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Green: Claim All button", nil, 0.7, "Loops remaining: " .. num_train)
+
+		num_train = num_train - 1;
+		if num_train <= 0 then
+		  lsPlaySound("complete.wav");
+		  sleepWithStatus(10000,"num Times to Upgrade has been met, aborting!");
+          error("num Times to Train has been met, aborting!");
+		end
       end
     end
+
+	if abort == 0 and not green_claim then
+   	  checkGrayButtons();
+	end
 end
 
 function checkGrayButtons()
+-- NOTE THIS FUNCTION IS COPY/PASTE FROM TROOP_UPGRADE.LUA - I AM NOT 100% SURE IF THIS CASE IS VALID OR NOT.
+-- FOR NOW, EVEN IF THERE ARE NO GRAY BUTTONS IT WILL STILL SERVE A PURPOSE BY ABORTING MACRO IF IT CAN'T FIND ANYTHING TO CLICK, FOR TOO LONG.
+-- IF checkedGray >= 4 (this function called to many times in a row) then it means we can't find anything else to click. Hence we are stuck; abort!
+
 -- A gray Use All (speeds) button MIGHT appear here.
 -- Check for Gray Use All button. This will appear when game is being stupid. Just close it out
 -- We will check for three possible Gray buttons. I've found that sometimes one button will not be found, when it should.
+  local foundGray = nil;
+  checkedGray = checkedGray +1;
 
-    if waitForImage("training/use_all_gray.png", waitTime, "Checking for GRAY USE ALL", nil, tolerance) then
-      sleepWithStatus(100,"ISSUE: Found Gray Use All Button: Preparing ESC KEY");
       UseAllGrayButton1 = srFindImage("training/use_all_gray.png")
       UseAllGrayButton2 = srFindImage("training/use_all_gray2.png")
-      UseAllGrayButton3 = srFindImage("training/use_all_gray3.png")
-      --Hit Esc key
+      --UseAllGrayButton3 = srFindImage("training/use_all_gray3.png")
+
+      if UseAllGrayButton1 or UseAllGrayButton2 or UseAllGrayButton3 then
+        foundGray = 1;
+      end
+
+      if foundGray or checkedGray >= 4 then
+         sleepWithStatus(1000,"ISSUE: Found Gray Use All Button or we are stuck: Using ESC KEY\n\nCheckedGray Counter: " .. checkedGray);
+	  EscapeKey(); -- Hit Esc Key
+      end
+
+      if checkedGray >= 10 then
+		 lsPlaySound("fail.wav");
+         error("Abort: Looped too many times without finding anything to click. Macro seems stuck on unexpected window");
+      end
+
+end
+
+function EscapeKey()
+      --Same as pressing Esc Key on keyboard
       srKeyDown(VK_ESCAPE);
       lsSleep(100);
       srKeyUp(VK_ESCAPE);
       lsSleep(100);
-   end
-
-  if not UseAllGrayButton1 and UseAllGrayButton2 then
-      if waitForImage("training/use_all_gray2.png", waitTime, "Checking for GRAY USE ALL 2", nil, tolerance) then
-        sleepWithStatus(100,"ISSUE: Found Gray Use All Button 2: Preparing ESC KEY");
-        --Hit Esc key
-        srKeyDown(VK_ESCAPE);
-        lsSleep(100);
-        srKeyUp(VK_ESCAPE);
-        lsSleep(100);
-     end
-  end
-
-  if not UseAllGrayButton1 and not UseAllGrayButton1 and UseAllGrayButton3 then
-      if waitForImage("training/use_all_gray3.png", waitTime, "Checking for GRAY USE ALL 3", nil, tolerance) then
-        sleepWithStatus(100,"ISSUE: Found Gray Use All Button 3: Preparing ESC KEY");
-        --Hit Esc key
-        srKeyDown(VK_ESCAPE);
-        lsSleep(100);
-        srKeyUp(VK_ESCAPE);
-        lsSleep(100);
-     end
-  end
 end
--- scripts/troop_upgrade.lua
+++ scripts/troop_upgrade.lua
@@ -1,23 +1,23 @@
 dofile("common.inc");
 dofile("constants.inc");
 
-waitTime = 100; -- How many ms (1000 = 1 second) to wait for an image to appear before giving up
+waitTime = 200; -- How many ms (1000 = 1 second) to wait for an image to appear before giving up
+defaultSleep = 200; -- How many ms for most sleep
 tolerance = 6000; -- Image tolerance to find it -- Default is 4500 when no tolerance is specified
 checkedGray = 0;
 abort = 0;
+num_loops = 0;
 
 function doit()
-
-  num_upgrades = promptNumber("Num times to Upgrade?", 1);
+  num_upgrades = promptNumber("Num times to Train/Upgrade Troops?", 1);
   askForWindow("Troop Upgrade\n\nSelect the troop you are upgrading so that the GREEN Upgrade button is showing BEFORE you begin!\n\nMouse over Blue Stacks window and press Shift key to continue.");  
-
   checkWindowSize();
 
   if waitForImage("training/upgrade_button.png", waitTime, "Verifying Green Upgrade button showing") then
-    sleepWithStatus(1500, "Green Upgrade button FOUND, continuing...", nil, 0.7)
+    sleepWithStatus(1500, "Green Upgrade button FOUND, continuing...", nil, 0.7);
     can_continue = 1;
   else
-    sleepWithStatus(2000, "Green Upgrade button NOT found, Shutting down!", nil, 0.7)
+    sleepWithStatus(2000, "Green Upgrade button NOT found, Shutting down!", nil, 0.7);
   end
 
   while can_continue and abort == 0 do
@@ -25,7 +25,7 @@ function doit()
     if waitForImage("training/upgrade_button.png", waitTime, "Verifying Green Upgrade button showing", nil, tolerance) then
       local upgrade = srFindImage("training/upgrade_button.png");
       srClickMouse(upgrade[0],upgrade[1]);
-      sleepWithStatus(100, "Clicking Upgrade button", nil, 0.7)
+      sleepWithStatus(defaultSleep, "Clicking Upgrade button", nil, 0.7, "Loops remaining: " .. num_upgrades)
     end
 
     -- Check Slider Bar before clicking Level up
@@ -33,7 +33,7 @@ function doit()
       local gold_plus = srFindImage("training/gold_plus.png");
       if gold_plus then
         srClickMouse(gold_plus[0]-52,gold_plus[1]+10);
-        sleepWithStatus(200, "Clicking Max on Slider Bar", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Max on Slider Bar", nil, 0.7, "Loops remaining: " .. num_upgrades)
       end
     end
 
@@ -42,20 +42,18 @@ function doit()
       local level_up = srFindImage("training/level_up.png");
       if level_up then
         srClickMouse(level_up[0],level_up[1]);
-        sleepWithStatus(100, "Clicking Level-Up button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Level-Up button", nil, 0.7, "Loops remaining: " .. num_upgrades)
       end
     end
 
-
-    checkGrayButtons()
-lsSleep(500);
+    checkGrayButtons();
 
     -- Check for Blue Use All button. This will appear if we don't have enough resources at hand
     if waitForImage("training/use_all_blue.png", waitTime, "Checking for USE ALL button", nil, tolerance) then
       local blue_use_all = srFindImage("training/use_all_blue.png");
       if blue_use_all then
         srClickMouse(blue_use_all[0],blue_use_all[1]);
-        sleepWithStatus(100, "Clicking Blue: Use All button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Blue: Use All button", nil, 0.7, "Loops remaining: " .. num_upgrades)
         checkedGray = 0;
       end
     end
@@ -66,7 +64,7 @@ lsSleep(500);
       local green_use_all = srFindImage("training/use_all_green.png");
       if green_use_all then
         srClickMouse(green_use_all[0],green_use_all[1]);
-        sleepWithStatus(100, "Clicking Green: Use All button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Green: Use All button", nil, 0.7, "Loops remaining: " .. num_upgrades)
         checkedGray = 0;
       end
     end
@@ -75,12 +73,13 @@ lsSleep(500);
       local blue_claim = srFindImage("training/claim_blue.png");
       if blue_claim then
         srClickMouse(blue_claim[0],blue_claim[1]);
-        sleepWithStatus(250, "Clicking Blue: Claim All button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Blue: Claim All button", nil, 0.7, "Loops remaining: " .. num_upgrades)
         checkedGray = 0;
         num_upgrades = num_upgrades - 1;
-	if num_upgrades <= 0 then
-          sleepWithStatus(2500,"num Times to Upgrade has been met, aborting!");
-          abort = 1;
+		if num_upgrades <= 0 then
+  		  lsPlaySound("complete.wav");
+          sleepWithStatus(10000,"num Times to Train has been met, aborting!");
+          error("num Times to Train has been met, aborting!");
         end
       end
     end
@@ -88,23 +87,21 @@ lsSleep(500);
 
     -- checkClaimGreen();
 
-
     if waitForImage("training/blue_speedup.png", waitTime, "Checking for Blue Speedup", nil, tolerance) then
       local blue_speed_up = srFindImage("training/blue_speedup.png");
       if blue_speed_up then
         srClickMouse(blue_speed_up[0],blue_speed_up[1]);
-        sleepWithStatus(100, "Clicking Blue: Speed Up button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Blue: Speed Up button", nil, 0.7, "Loops remaining: " .. num_upgrades)
         checkedGray = 0;
       end
     end
-
 
     -- Sometimes a popup "The Time exceeded 00:xx:xx will be wasted after using Auto Speedup. Confirm to use?"
     if waitForImage("training/confirm_yes.png", waitTime, "Checking Confirm Speedup is Wasted", nil, tolerance) then
       local confirm_yes = srFindImage("training/confirm_yes.png");
       if confirm_yes then
         srClickMouse(confirm_yes[0],confirm_yes[1]);
-        sleepWithStatus(100, "Clicking Confirm Yes button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Confirm Yes button", nil, 0.7, "Loops remaining: " .. num_upgrades)
       end
     end
   end -- while
@@ -116,19 +113,17 @@ function checkClaimGreen()
       local green_claim = srFindImage("training/claim_green.png");
       if green_claim then
         srClickMouse(green_claim[0],green_claim[1]);
-        sleepWithStatus(250, "Clicking Green: Claim All button", nil, 0.7)
+        sleepWithStatus(defaultSleep, "Clicking Green: Claim All button", nil, 0.7, "Loops remaining: " .. num_upgrades)
         num_upgrades = num_upgrades - 1;
-	if num_upgrades <= 0 then
+		if num_upgrades <= 0 then
           sleepWithStatus(2500,"num Times to Upgrade has been met, aborting!");
           abort = 1;
         end
       end
 
-      --Hit Esc key
-      srKeyDown(VK_ESCAPE);
-      lsSleep(100);
-      srKeyUp(VK_ESCAPE);
-      lsSleep(100);
+	  EscapeKey(); -- Hit Esc Key
+      lsSleep(defaultSleep);
+	  EscapeKey(); -- Hit Esc Key
       error("Quitting; ALL TROOPS UPGRADED!?");
     end
 end
@@ -145,20 +140,25 @@ function checkGrayButtons()
       --UseAllGrayButton3 = srFindImage("training/use_all_gray3.png")
 
       if UseAllGrayButton1 or UseAllGrayButton2 or UseAllGrayButton3 then
-        foundGray = 1
+        foundGray = 1;
       end
 
       if foundGray or checkedGray >= 4 then
-         sleepWithStatus(100,"ISSUE: Found Gray Use All Button: Using ESC KEY\n\nCheckedGray Counter: " .. checkedGray);
-        --Hit Esc key
-        srKeyDown(VK_ESCAPE);
-        lsSleep(100);
-        srKeyUp(VK_ESCAPE);
-        lsSleep(100);
+         sleepWithStatus(1000,"ISSUE: Found Gray Use All Button or we are stuck: Using ESC KEY\n\nCheckedGray Counter: " .. checkedGray);
+	  EscapeKey(); -- Hit Esc Key
       end
 
       if checkedGray >= 10 then
-         abort = 1;
+  		 lsPlaySound("fail.wav");
+         error("Abort: Looped too many times without finding anything to click. Macro seems stuck on unexpected window");
       end
 
+end
+
+function EscapeKey()
+      --Same as pressing Esc Key on keyboard
+      srKeyDown(VK_ESCAPE);
+      lsSleep(100);
+      srKeyUp(VK_ESCAPE);
+      lsSleep(100);
 end
